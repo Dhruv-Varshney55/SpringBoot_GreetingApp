b186720a5607b64e9ea7113cd3bc0ef43d0dba8b
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2025-03-04 16:13:37 +0530

    UC8

--- src/main/java/com/example/GreetingApp/service/GreetingService.java
+++ src/main/java/com/example/GreetingApp/service/GreetingService.java
@@ -3,7 +3,10 @@ package com.example.GreetingApp.service;
 import com.example.GreetingApp.model.Greeting;
 import com.example.GreetingApp.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -41,5 +44,13 @@ public class GreetingService {
             return greetingRepository.save(greeting);
         }
         throw new RuntimeException("Greeting with ID " + id + " not found");
+    }
+
+    // UC8
+    public void deleteGreeting(Long id) {
+        if (!greetingRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Greeting not found!");
+        }
+        greetingRepository.deleteById(id);
     }
 }
